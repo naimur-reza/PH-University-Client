40b4794e2e3088687f1c020d43cded85449805ac
Naimur Reza <naimur.rezaa@gmail.com>
2024-02-04 22:00:31 +0600

    Add academic routes

--- src/routes/admin.routes.tsx
+++ src/routes/admin.routes.tsx
@@ -1,4 +1,9 @@
+import AcademicDepartment from "../pages/Admin/AcademicManagement/AcademicDepartment";
+import AcademicFaculty from "../pages/Admin/AcademicManagement/AcademicFaculty";
 import AcademicSemester from "../pages/Admin/AcademicManagement/AcademicSemester";
+import CreateAcademicDepartment from "../pages/Admin/AcademicManagement/CreateAcademicDepartment";
+import CreateAcademicFaculty from "../pages/Admin/AcademicManagement/CreateAcademicFaculty";
+import CreateAcademicSemester from "../pages/Admin/AcademicManagement/CreateAcademicSemester";
 import AdminDashboard from "../pages/Admin/AdminDashboard";
 import CreateAdmin from "../pages/Admin/CreateAdmin";
 import CreateFaculty from "../pages/Admin/CreateFaculty";
@@ -9,6 +14,42 @@ export const adminPaths = [
     name: "Dashboard",
     path: "dashboard",
     element: <AdminDashboard />,
+  },
+
+  {
+    name: "Academic Management",
+    children: [
+      {
+        name: "Create A. Semester",
+        path: "create-academic-semester",
+        element: <CreateAcademicSemester />,
+      },
+      {
+        name: "Academic Semester",
+        path: "academic-semester",
+        element: <AcademicSemester />,
+      },
+      {
+        name: "Create A. Faculty",
+        path: "create-academic-faculty",
+        element: <CreateAcademicFaculty />,
+      },
+      {
+        name: "Academic Faculty",
+        path: "academic-faculty",
+        element: <AcademicFaculty />,
+      },
+      {
+        name: "Create A. Department",
+        path: "create-academic-department",
+        element: <CreateAcademicDepartment />,
+      },
+      {
+        name: "Academic Department",
+        path: "academic-department",
+        element: <AcademicDepartment />,
+      },
+    ],
   },
   {
     name: "User Management",
@@ -32,16 +73,6 @@ export const adminPaths = [
         name: "Create Member",
         path: "create-member",
         element: <CreateStudent />,
-      },
-    ],
-  },
-  {
-    name: "Academic Management",
-    children: [
-      {
-        name: "Academic Semester",
-        path: "academic-semester",
-        element: <AcademicSemester />,
       },
     ],
   },
